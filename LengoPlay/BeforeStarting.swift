e8b2c60248ecfef826811ffc94041fff3aafd612
ghidaa <ghidaafarhan@gmail.com>
2024-01-30 15:14:09 -0500

    make sure you got the BeforeStarting File

---
+++ LengoPlay/BeforeStarting.swift
@@ -0,0 +1,18 @@
+//
+//  BeforeStarting.swift
+//  LengoPlay
+//
+//  Created by Ghaida Farhan on 1/30/24.
+//
+
+import SwiftUI
+
+struct BeforeStarting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BeforeStarting()
+}
--- LengoPlay/ContentView.swift
+++ LengoPlay/ContentView.swift
@@ -11,21 +11,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Image("Background")
-                .resizable()
-                .ignoresSafeArea()
-                .frame(width:500)
-            VStack{
-                SlowPrint(printedText: "WELCOME")
-                    .font(Font.custom("Merriweather-BoldItalic", size: 60))
-                    .foregroundColor(.ourBlue)
+        NavigationStack{
+            ZStack{
+                Image("Background")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .frame(width:500)
+                VStack(spacing: 60){
+                    SlowPrint(printedText: "WELCOME")
+                        .font(Font.custom("Merriweather-BoldItalic", size: 60))
+                        .foregroundColor(.ourBlue)
+                    
+                    Text("Ready to level-up?")
+                        .font(Font.custom("AkayaTelivigala-Regular", size: 27))
+                        .foregroundColor(.ourBlue)
+                    
+                    NavigationLink{
+                        BeforeStarting()
+                        
+                    } label: {
+                        Label("Let's go!",systemImage: "")
+                            .font(Font.custom("AkayaTelivigala-Regular", size: 50))
+                            .frame(width:240,height:70)
+                            .padding(20)
+                            .foregroundColor(.ourBegie)
+                            .background(Color.ourGreen)
+                            .cornerRadius(19)
+                            
+                        
+                    }
+                    
+                    
+                    
+                    
+                }
             }
-            
-            
-            
-          
-            
         }
     }
 }
