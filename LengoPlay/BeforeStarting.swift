ecf7d42012ccf6f3630e4d0c4ba76d718336d171
Asif <md.mushfikhasanasif@gmail.com>
2024-02-01 16:47:37 -0500

    Language and Next

--- LengoPlay/BeforeStarting.swift
+++ LengoPlay/BeforeStarting.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct BeforeStarting: View {
+    let languages = ["Arabic","Bengali","",""]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+                .frame(width: 500)
+            
+            VStack(spacing: 20) {
+                Text("Before you start...")
+                    .font(Font.custom("Merriweather-BoldItalic", size: 30))
+                    .foregroundColor(.ourBlue)
+                
+                Text("Select your language:")
+                    .font(Font.custom("Merriweather-BoldItalic", size: 30))
+                    .foregroundColor(.ourBlue)
+                
+                HStack {
+                    ZStack {
+                        Text("Language")
+                            .font(Font.custom("AkayaTelivigala-Regular", size: 30))
+                            .frame(width: 330, height: 20)
+                            .foregroundColor(.ourBegie)
+                            .padding(20)
+                            .background(Color.ourGreen)
+                            .cornerRadius(10)
+                    
+                        HStack {
+                        Spacer()
+                            Image(systemName: "chevron.right")
+                                .font(Font.system(size: 30))
+                                .foregroundColor(.ourBlue)
+                                .padding(.trailing, 10)
+                        }
+                    }
+                }
+                .padding(.bottom, 20)
+                
+                Spacer()
+                
+                Label("Next..", systemImage: "")
+                    .font(Font.custom("AkayaTelivigala-Regular", size: 40))
+                    .frame(width: 200, height: 50)
+                    .padding(20)
+                    .foregroundColor(.ourBegie)
+                    .background(Color.ourGreen)
+                    .cornerRadius(19)
+            }
+        }
     }
 }
 
-#Preview {
-    BeforeStarting()
+struct NextView: View {
+    var body: some View {
+        Text("Next View")
+    }
+}
+
+struct BeforeStarting_Previews: PreviewProvider {
+    static var previews: some View {
+        BeforeStarting()
+    }
 }
---
+++ LengoPlay/next.swift
@@ -0,0 +1,18 @@
+//
+//  next.swift
+//  LengoPlay
+//
+//  Created by Asif on 2/1/24.
+//
+
+import SwiftUI
+
+struct next: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    next()
+}
